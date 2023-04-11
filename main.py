f4b6fec74e5bb9d89e94976ea29d509445a6709e
JeiversonChristian <jeiversonc@gmail.com>
2023-04-11 08:10:22 -0300

    creating ask basis function and reformation the other function

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-while True:
-    number = input("Digite um número natural: ")
-    if number.isdigit() == False:
-        print("Apenas números naturais!")
-    else:
-        break
-print(number)
+def ask_number():
+    while True:
+        number = input("Digite um número natural: ")
+        if number.isdigit() == False:
+            print("Apenas números naturais, por favor.")
+        else:
+            break
+    print(number)
+
+def ask_base():
+    while True:
+        print("1 - Decimal | 2 - Binária | 3 - Octal | 4 - Hexadecimal")
+        base = input("Digite apenas o número da opção correspondente à base do número que você digitou: ")
+        if base!="1" and base!="2" and base!="3" and base!="4":
+            print("Não entend. Por favor, digite a apenas o número da base do seu número.")
+        else:
+            break
+    print(base)
+
+ask_number()
+ask_base()
