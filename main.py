7ba1e102b906d2df24ef37765b1f612867d61e8d
JeiversonChristian <jeiversonc@gmail.com>
2023-04-14 16:31:07 -0300

    stat to create front and back-end

---
+++ main.py
@@ -0,0 +1,9 @@
+# Back-end
+
+# I used kivy library to create the graphic interface of this application 
+# It was necessary to installed it: pip install kivy
+
+# To import the App
+# To import the Builder - that will allow to connect the screen.kv to main.py
+# To creat the App
+# To creat the build function
